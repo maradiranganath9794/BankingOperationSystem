6406a9bc59a7fcc57c734dbee22402451dff1fea
root <root@ip-172-31-33-73.ap-south-1.compute.internal>
2026-03-05 10:46:14 +0000

    python file modified

--- SavingAccount.py
+++ SavingAccount.py
@@ -3,6 +3,7 @@ class SavingAccount(BankAccount):
     def deposit(self,amount):
         super().deposit(amount)
         self.current_balance += amount * 0.03
-        print(f"Added interest to Account New Balance: {self.current_balance}")
+        print(f"Added interest to Account New Balance: {self.current_balance}")i
+        print("hello")
 
 
